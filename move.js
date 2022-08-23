52275677ec1db444b970b82047029fc70bf31023
dgraham742 <106363652+dgraham742@users.noreply.github.com>
2022-08-22 20:10:11 -0500

    commit

--- move.js
+++ move.js
@@ -1,5 +1,12 @@
-var apiKey="4fedd68b8e63edc633d408c361a04e03"
-var apiUrl =https//api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={apiKey}
+var apiKey="b15e816cdb167f0393835f60ed447f93" 
+var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={apiKey}"
+var searchFunction =document.querySelector("#card")
+var geoApiKey = "c3940598db6d977ef0a58084b53752d8"
+var geoLocation = "http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}"
+
+    
+
+
 function getWeatherApi(cityName) {
     fetch(apiUrl)
         
@@ -8,6 +15,4 @@ function getWeatherApi(cityName) {
     })
     .then (function(data) {
         console.log(data);
-    });
-}
-
+    })}
